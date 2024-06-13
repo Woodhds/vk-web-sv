cbd71336ff0a9f4897cea8c60d218df7fbab0583
sergey.borodin <sergey.borodin@aliexpress.ru>
2024-06-13 10:15:22 +0300

    add lazy load

--- src/lib/components/CardImage.svelte
+++ src/lib/components/CardImage.svelte
@@ -1,51 +1,46 @@
 <script lang="ts">
-    let {images = $bindable(), key}: { images: string[], key: string } = $props();
-
-    let imgs = $state([] as string[])
-
-    let carousel: HTMLElement = null;
-    $effect(() => {
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-
-                    imgs = [...images || []]
-                    observer.disconnect()
-                }
-            })
-        }, {
-            threshold: 0.05
-        })
-
-        observer.observe(carousel)
-
-        return () => {
-            observer.disconnect()
-        }
-    })
-
+    let {
+        images = $bindable(),
+        key = $bindable(),
+    }: { images: string[]; key: string } = $props();
 </script>
 
-<div bind:this={carousel} class="carousel">
-    {#if imgs !== undefined && imgs !== null && imgs.length > 0}
-        {#each imgs as image, i}
-            <div id="{key + '-' + i}" class="carousel-item relative w-full justify-center">
-                <img src="{image}" class="rounded-2xl h-72 w-auto"/>
+<div class="carousel">
+    {#if images.length > 0}
+        {#each images as image, i}
+            <div
+                id={key + "-" + i}
+                class="carousel-item relative w-full justify-center"
+            >
+                <img
+                    src={image}
+                    class="rounded-2xl h-72 w-auto"
+                    loading="lazy"
+                    alt={key}
+                />
                 {#if images.length > 1}
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#{key + '-' + (i - 1)}" class="btn btn-circle bg-transparent text-white">❮</a>
-                        <a href="#{key + '-' + (i + 1)}" class="btn btn-circle bg-transparent text-white">❯</a>
+                    <div
+                        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+                    >
+                        <a
+                            href="#{key + '-' + (i - 1)}"
+                            class="btn btn-circle bg-transparent text-white"
+                            >❮</a
+                        >
+                        <a
+                            href="#{key + '-' + (i + 1)}"
+                            class="btn btn-circle bg-transparent text-white"
+                            >❯</a
+                        >
                     </div>
                 {/if}
             </div>
         {/each}
-
     {:else}
         <div class="carousel-item relative w-full justify-center">
             <svg height="300" width="400" viewBox="0 0 496.158 496.158">
                 <use xlink:href="#no-photo"></use>
             </svg>
         </div>
-
     {/if}
 </div>
